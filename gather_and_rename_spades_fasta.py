13026b202b4fcc354fb1c0b7f785f72b8d5dc877
thej-ssi <thej@ssi.dk>
2020-04-07 21:44:46 +0200

    rename_spades

---
+++ gather_and_rename_spades_fasta.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+in_dir = sys.argv[1]
+out_dir = sys.argv[2]
+
+files  = os.path.join(in_dir)
+
+if not os.path.exists(out_dir):
+	os.makedirs(out_dir)
+
+for file in files:
+	file_split = file.split('_')
+	ID = '_'.join(file_split[:-2])
+	in_file = os.path.join(in_dir,file,'contigs.fasta')
+	printline = ''
+	with open(in_file) as f:
+		for line in f:
+			if line[0] == '>':
+				printline += '>'+ID+'__'+line[1:]
+				print(printline)
+			else:
+				printline += line
+	out_file = os.path.join(out_dir,ID+'.fasta')
+	o = open(out_file,'w')
+	o.write(printline)
+	o.close()
