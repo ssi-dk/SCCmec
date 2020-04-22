2933cb7bebd0bf4ad7e04e927eca42796be1deac
thej-ssi <thej@ssi.dk>
2020-04-22 15:20:15 +0200

    Update db_rename.py

--- db_rename.py
+++ db_rename.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-#o = open(sys.argv[2],'w')
+o = open(sys.argv[2],'w')
 with open(sys.argv[1]) as f:
 	for line in f:
 		if line[0] == '>':
@@ -11,6 +11,8 @@ with open(sys.argv[1]) as f:
 			splitline = splitline[1].split(']')
 			loc_string = splitline[0]
 			ID = line.split(' ')[0]
-			header = ID+'__loc'+loc_string
-			print(header)
-#o.close()
+			header = ID+'__loc'+loc_string+'\n'
+			o.write(header)
+		else:
+			o.write(line)
+o.close()
